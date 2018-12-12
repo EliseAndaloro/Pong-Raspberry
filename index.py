16b24eb900e2e2e0f1db0415e00cba62fa1c7c3c
Unknown <elise.andaloro@hotmail.fr>
2018-12-12 13:45:26 +0100

    AfficherRaquette

--- index.py
+++ index.py
@@ -10,18 +10,28 @@ y=random.randint(1,6)
 x_sens=1
 y_sens=1
 
+rx=0
+ry=0
 
+ry_sens=1
+
+def afficheRaquette():
+  sense.set_pixel(rx,ry, 242, 62, 104)
+  sense.set_pixel(rx,ry_sens, 242,62,104)
+  sense.set_pixel(rx,ry_sens+1, 242,62,104)
+  
+  
 sense.set_pixel(x,y,66, 134, 244)
 while True:
+    
     sense.clear(0,0,0)
+    afficheRaquette()
     sense.set_pixel(x,y,66,134,244)
     time.sleep(0.2)
     x = x+x_sens
     y=y+y_sens
     
-    
     if x==7:
-      
       x_sens=-1
     
     if y==0:
@@ -33,3 +43,4 @@ while True:
     
     if x==0:
       x_sens=1
+      
