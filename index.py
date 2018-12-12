18da401b57576f3ab23b2218dec88bb4e1f1048b
Unknown <elise.andaloro@hotmail.fr>
2018-12-12 12:26:50 +0100

    Faire Rebondir la balle sur les murs

--- index.py
+++ index.py
@@ -1,13 +1,35 @@
 from sense_hat import SenseHat
 sense = SenseHat()
 import time
+import random
 sense.clear(0,0,0)
-x=0
-y=6
+
+x=random.randint(1,6)
+y=random.randint(1,6)
+
+x_sens=1
+y_sens=1
+
+
 sense.set_pixel(x,y,66, 134, 244)
-while x<7:
+while True:
     sense.clear(0,0,0)
     sense.set_pixel(x,y,66,134,244)
-    time.sleep(0.5)
-    x= x+1
-    y=y-1
+    time.sleep(0.2)
+    x = x+x_sens
+    y=y+y_sens
+    
+    
+    if x==7:
+      
+      x_sens=-1
+    
+    if y==0:
+      y_sens=1 
+
+      
+    if y==7:
+      y_sens=-1
+    
+    if x==0:
+      x_sens=1
