d1fe18c16fdb58161ac59bc769e49757628046d3
Unknown <elise.andaloro@hotmail.fr>
2018-12-12 16:17:52 +0100

    Pong Fonctionnel

--- index.py
+++ index.py
@@ -1,8 +1,11 @@
-from sense_hat import SenseHat
+from sense_emu import SenseHat
+
 sense = SenseHat()
 import time
 import random
 sense.clear(0,0,0)
+
+
 
 x=random.randint(1,6)
 y=random.randint(1,6)
@@ -13,20 +16,34 @@ y_sens=1
 rx=0
 ry=0
 
-ry_sens=1
+
 
 
 def afficheRaquette():
   sense.set_pixel(rx,ry, 242, 62, 104)
-  sense.set_pixel(rx,ry_sens, 242,62,104)
-  sense.set_pixel(rx,ry_sens+1, 242,62,104)
+  sense.set_pixel(rx,ry+1, 242,62,104)
+  sense.set_pixel(rx,ry+2, 242,62,104)
 
-  
-  
+
+def descendre():
+  global ry
+  if ry+2<7:
+    ry=ry+1
+
+sense.stick.direction_down = descendre
+
+def monter():
+  global ry
+  if ry>0:
+    ry=ry-1
+
+sense.stick.direction_up=monter
+
+ 
+
 sense.set_pixel(x,y,66, 134, 244)
 
 while True:
-    
     sense.clear(0,0,0)
     afficheRaquette()
     sense.set_pixel(x,y,66,134,244)
@@ -47,13 +64,13 @@ while True:
     if x==1 and y==ry:
       x_sens=1
       
-    if x==1 and y==ry_sens:
+    if x==1 and y==ry+1:
       x_sens=1
       
-    if x==1 and y==ry_sens+1:
+    if x==1 and y==ry+2:
       x_sens=1
     
-    if x==1 and y ==ry_sens+2:
+    if x==1 and y ==ry+3:
       x_sens=1
       
     if x==0:
@@ -61,3 +78,13 @@ while True:
       sense.set_pixel(x,y,34, 206, 8)
       afficheRaquette()
       break
+  
+
+  
+  
+  
+  
+  
+      
+      
+      
