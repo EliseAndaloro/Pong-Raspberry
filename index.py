b90d4d663731135e2cc43761b0e6547443bc11b7
Unknown <elise.andaloro@hotmail.fr>
2018-12-12 10:22:45 +0100

    FaireBougerLaLedsur1Ligne

--- index.py
+++ index.py
@@ -1,7 +1,12 @@
-###Affichage d'un seul pixel###
 from sense_hat import SenseHat
 sense = SenseHat()
+import time
 sense.clear(0,0,0)
-x=4
+x=0
 y=6
 sense.set_pixel(x,y,66, 134, 244)
+while x<8:
+    sense.clear(0,0,0)
+    sense.set_pixel(x,y,66,134,244)
+    time.sleep(0.5)
+    x= x+1
