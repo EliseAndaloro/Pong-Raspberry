6f5e716c77d34ec343578b1c570a802917034729
Unknown <elise.andaloro@hotmail.fr>
2018-12-12 10:23:23 +0100

    faireBougerEnDiagonale

--- index.py
+++ index.py
@@ -5,8 +5,9 @@ sense.clear(0,0,0)
 x=0
 y=6
 sense.set_pixel(x,y,66, 134, 244)
-while x<8:
+while x<7:
     sense.clear(0,0,0)
     sense.set_pixel(x,y,66,134,244)
     time.sleep(0.5)
     x= x+1
+    y=y+1
